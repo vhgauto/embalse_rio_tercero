d6bcf04e5af75e5fae6efbe03638dab63b6cbfa3
vhgauto <victor.gauto@outlook.com>
2025-12-03 19:02:07 -0300

    quito comentarios

--- scripts/lectura_cota.R
+++ scripts/lectura_cota.R
@@ -1,5 +1,7 @@
 # https://cordoba.redesclimaticas.com/next/reports?mss=30335
 
+library(tidyverse)
+
 lista_xlsx <- list.files(
   "datos/",
   pattern = "resumenes_diarios",
@@ -15,30 +17,6 @@ f_cota <- function(H) {
 cota_tbl <- map(lista_xlsx, f_cota) |>
   list_rbind()
 
-
-# a1 <- readxl::read_xlsx(
-#   "datos/Base_ERT_OriginalActualizada.xlsx",
-#   sheet = "3-Nivel_Diques-Cba"
-# ) |>
-#   select(fecha = 1, altura = "Embalse") |>
-#   filter(fecha != "h labio de Vertedero") |>
-#   drop_na() |>
-#   mutate(fecha = janitor::excel_numeric_to_date(as.numeric(fecha)))
-
-# a2 <- readxl::read_xlsx(
-#   "datos/resumenes_diarios_2025-09-01_2025-10-01.xlsx",
-#   skip = 5
-# ) |>
-#   select(fecha = 1, altura = 3) |>
-#   mutate(fecha = dmy(fecha))
-
-# rbind(a1, a2) |>
-#   arrange(desc(fecha)) |>
-#   mutate(año = year(fecha)) |>
-#   filter(año == 2025) |>
-#   mutate(mes = month(fecha)) |>
-#   count(mes)
-
 base_de_datos_cota <- read_csv(
   "datos/base_de_datos_cotas.csv",
   show_col_types = FALSE
@@ -50,26 +28,3 @@ if (FALSE) {
     distinct() |>
     write_csv("datos/base_de_datos_cotas.csv")
 }
-
-#
-
-# XXX LECTURA DE DATOS
-
-# cotas_l <- list.files(
-#   "datos/",
-#   pattern = "resumenes_diarios_",
-#   full.names = TRUE
-# )
-
-# f_cota <- function(H) {
-#   readxl::read_xlsx(H, skip = 5) |>
-#     select(fecha = 1, altura = 3) |>
-#     mutate(fecha = dmy(fecha))
-# }
-
-# cota_actualizado <- map(cotas_l, f_cota) |>
-#   list_rbind()
-
-# read_csv("datos/base_de_datos_cotas.csv", show_col_types = FALSE) |>
-#   rbind(cota_actualizado) |>
-#   distinct()
