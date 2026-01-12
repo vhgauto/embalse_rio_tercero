918754dbc292f2f5c6df8ce4cb80ddecdd21822b
vhgauto <victor.gauto@outlook.com>
2026-01-12 13:44:22 -0300

    millones en eje y, quito espacios, agrego escala

--- scripts/figura_ciano_mensual.R
+++ scripts/figura_ciano_mensual.R
@@ -1,4 +1,3 @@
-
 # datos -------------------------------------------------------------------
 
 algas_orden <- c("Diatomeas", "Clorofitas", "Cianobacterias", "Criptófitas")
@@ -9,37 +8,50 @@ d_micro <- read_csv("datos/base_de_datos_micro.csv", show_col_types = FALSE) |>
 
 d_micro_suma <- reframe(d_micro, s = sum(suma), .by = fecha)
 
+escala_ciano <- 1e6
+
+max_ciano_y <- ceiling(max(d_micro_suma$s) / escala_ciano) * escala_ciano
+
 # figura ------------------------------------------------------------------
 
 g_ciano_mensual <- ggplot(d_micro, aes(fecha, suma, fill = algas)) +
   geom_col(position = position_stack()) +
-
   geom_line(
-    data = d_micro_suma, aes(fecha, s), inherit.aes = FALSE, color = "black",
+    data = d_micro_suma,
+    aes(fecha, s),
+    inherit.aes = FALSE,
+    color = "black",
     linewidth = .5
   ) +
   geom_point(
-    data = d_micro_suma, aes(fecha, s), inherit.aes = FALSE, size = 2,
-    shape = 21, fill = "white", color = "black", stroke = .7
+    data = d_micro_suma,
+    aes(fecha, s),
+    inherit.aes = FALSE,
+    size = 2,
+    shape = 21,
+    fill = "white",
+    color = "black",
+    stroke = .7
   ) +
-
   scale_x_date(
     breaks = scales::breaks_width("1 month"),
     labels = \(x) str_to_sentence(format(x, "%b '%y"))
   ) +
   scale_y_continuous(
     expand = expansion(mult = c(0, .05), add = c(0, 0)),
-    breaks = scales::breaks_width(1e6),
+    breaks = scales::breaks_width(2 * escala_ciano),
+    limits = c(0, max_ciano_y),
     labels = \(Q) {
       if_else(
         Q == 0,
         "0",
-        scales::label_number(
-          big.mark = ",",
-          decimal.mark = ".",
-          suffix = "E+6",
-          scale = 1e-6
-        )(Q)
+        # scales::label_number(
+        #   big.mark = ",",
+        #   decimal.mark = ".",
+        #   suffix = "E+6",
+        #   scale = 1e-6
+        # )(Q)
+        scales::label_number(scale = 1 / escala_ciano)(Q)
       )
     }
   ) +
@@ -48,7 +60,7 @@ g_ciano_mensual <- ggplot(d_micro, aes(fecha, suma, fill = algas)) +
     values = c("#F4A261", "#FFFF00", "#2A9D8F", "#B7DEE8")
   ) +
   labs(
-    y = "Abundancia (cel/L)",
+    y = "Abundancia (millones de cel/L)",
     x = NULL,
     fill = NULL
   ) +
