eb21febf698415fee13d560090b2a4766c1c809d
vhgauto <victor.gauto@outlook.com>
2026-01-12 13:45:00 -0300

    reordeno, agrego documento

--- scripts/generar_docx.R
+++ scripts/generar_docx.R
@@ -1,6 +1,6 @@
-informe(MES = 12, AÑO = 2025)
+mes_actual <- 1
+año_actual <- 2026
 
-mes_actual <- 12
-año_actual <- 2025
+informe(MES = mes_actual, AÑO = año_actual)
 
-browseURL("docs/informe_docx_2025_12.docx")
+browseURL(documento())
